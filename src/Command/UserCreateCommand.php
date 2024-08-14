503a84929f312e22eba0f17c7b802c0bd7a30b86
Warez-amasya <thimote.cabotte6259@gmail.com>
2024-08-14 14:16:32 +0200

    login check

---
+++ src/Command/UserCreateCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(name: 'app:user:create-user', description: 'Create user for test',)]
+class UserCreateCommand extends Command {
+
+    public function __construct(
+        private readonly UserPasswordHasherInterface $hasher,
+        private readonly EntityManagerInterface $manager
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $user = new User();
+        $user->setUsername("Warez")
+             ->setEmail("warez@gmail.com")
+             ->setRoles(["ROLE_ADMIN"]);
+
+        $hashPassword = $this->hasher->hashPassword($user, "warez1234");
+        $user->setPassword($hashPassword);
+
+        $this->manager->persist($user);
+        $this->manager->flush();
+
+        $io->success("L'utilisateur à bien été créer !");
+        return Command::SUCCESS;
+    }
+}
